10e6d52ef8ce992b5509dbed386b04a608a0e8a2
YuriMatheusM <158796863+YuriMatheusM@users.noreply.github.com>
2024-09-13 11:23:37 -0300

    Sequencia de Fibonacci usando java na versão1.8

---
+++ SequenciaDeFibonacci.java
@@ -0,0 +1,39 @@
+
+package sequencia.de.fibonacci;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SequenciaDeFibonacci {
+
+
+    public static void main(String[] args) {
+        List num = new ArrayList<>();
+        Scanner pergun = new Scanner(System.in);
+        int sequencia = 0;
+        int anterior = 0;
+        int resp;
+        for (int i =0; i<30; i++ ) {
+          if(i==0){
+              sequencia += 1;
+              anterior = 0;
+          }else{
+              sequencia += anterior;
+              anterior = sequencia - anterior;
+              
+          }
+        num.add(sequencia);
+        
+        }
+        System.out.println("Informe um numero nque deseja encontrar na sequencia de Fibonacci");
+        resp = pergun.nextInt();
+        if(num.contains(resp)){
+            System.out.println("O numero "+resp+" se encontra na sequencia de Fibonacci");
+            System.out.println("Aqui esta alguns dos numeros da seqiencia: "+num);
+        }else{
+            System.out.println("O numero informado nao se encontra na squencia.");
+        }
+    
+    }
+}
